bdb24bc620fbb2257ce1d6d0a6b29805d0ddedec
Dr_Romantic <45228785+rkdrns4747@users.noreply.github.com>
2020-09-17 11:46:36 +0900

    - Changed implemention behavior to API. - Fixed error when boost time has not been set.

--- src/main/java/org/dr_romantic/jbi/main/JobsBoostIndicator.java
+++ src/main/java/org/dr_romantic/jbi/main/JobsBoostIndicator.java
@@ -2,6 +2,9 @@ package org.dr_romantic.jbi.main;
 
 
 import me.TechsCode.UltraPermissions.UltraPermissions;
+import me.TechsCode.UltraPermissions.UltraPermissionsAPI;
+import me.TechsCode.UltraPermissions.base.storage.Stored;
+import me.TechsCode.UltraPermissions.storage.objects.Group;
 import me.TechsCode.UltraPermissions.storage.objects.Permission;
 import me.TechsCode.UltraPermissions.storage.objects.User;
 import org.bukkit.Bukkit;
@@ -12,10 +15,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.awt.Color;
 import java.util.List;
+import java.util.Set;
 
 public class JobsBoostIndicator extends JavaPlugin {
     public boolean availability = true;
     private final String prefix = net.md_5.bungee.api.ChatColor.of(new Color(0, 128, 255)) + "[ "+ChatColor.GREEN + "지구렐름 "+net.md_5.bungee.api.ChatColor.of(new Color(0, 128, 255)) + "]";
+    UltraPermissionsAPI upermAPI;
     public static void main(String[] args){
 
     }
@@ -26,6 +31,9 @@ public class JobsBoostIndicator extends JavaPlugin {
         if(!Bukkit.getPluginManager().isPluginEnabled("UltraPermissions")){
             Bukkit.getConsoleSender().sendMessage(ChatColor.RED +"[WARNING] Cannot find UltraPermissions! This plugin would not work without UntraPermissions plugin.");
             availability = false;
+        }
+        if(availability){
+            upermAPI = UltraPermissions.getAPI();
         }
     }
 
@@ -47,23 +55,28 @@ public class JobsBoostIndicator extends JavaPlugin {
         }else {
             name = ((Player) sender).getName();
         }
-        UltraPermissions uperm = new UltraPermissions((JavaPlugin) Bukkit.getPluginManager().getPlugin("UltraPermissions"));
-        User user = uperm.getUsers().name(name).orElse(null);
+        User user = upermAPI.getUsers().name(name).orElse(null);
         if(user == null){
             return true;
         }
-        List<Permission> userPerms = user.getPermissions();
-        boolean nowIn = false;
-        for(Permission each : userPerms){
-            if(each.getName().contains("jobs.boost.all.all.")){
-                String date;
-                if(each.getExpiration() == 0) //check if this permission is permanent
-                    date = ChatColor.translateAlternateColorCodes('&', "&2&l무제한");
-                else
-                    date = new java.text.SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new java.util.Date(each.getExpiration()));
+        //List<Permission> userPerms = user.getPermissions();
+        Set<Stored<Group>> storedGroupSet = user.getGroups();
 
+        boolean nowIn = false;
+        for(Stored<Group> each : storedGroupSet){
+            Group group = each.orElse(null);
+            if(group.getName().equalsIgnoreCase("jobs_boost")){
+                String date;
+                long expiry = user.getGroupExpiry(each);
+                if(expiry == 0) { //check if this permission is permanent
+                    date = ChatColor.translateAlternateColorCodes('&', "&2&l무제한");
+                    sender.sendMessage(prefix+ net.md_5.bungee.api.ChatColor.GOLD + " 현재 이용 중인 직업 부스트의 만기: " +net.md_5.bungee.api.ChatColor.of(new Color(0,255,182)) + date);
+                    return true;
+                }else {
+                    date = new java.text.SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new java.util.Date(expiry));
+                }
                 sender.sendMessage(prefix+ net.md_5.bungee.api.ChatColor.GOLD + " 현재 이용 중인 직업 부스트의 만기: " +net.md_5.bungee.api.ChatColor.of(new Color(0,255,182)) + date);
-                long gap = (each.getExpiration() - System.currentTimeMillis()) / 1000;
+                long gap = (expiry - System.currentTimeMillis()) / 1000;
                 long days = gap / 86400;
                 long hours = (gap - (days * 86400)) / 3600;
                 long minutes = (gap - (days * 86400) - (hours * 3600)) / 60;
